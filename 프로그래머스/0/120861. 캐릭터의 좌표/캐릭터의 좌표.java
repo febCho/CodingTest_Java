64ea4001940205767e936929ccf408ba07cf3753
Feb <144503463+febCho@users.noreply.github.com>
2024-05-22 10:45:55 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.03 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] solution(String[] keyinput, int[] board) {
+        int[] answer = {0, 0};
+        int maxX = board[0] / 2;
+        int maxY = board[1] / 2;
+        
+        for (String key : keyinput) {
+            switch (key) {
+                case "up":
+                    if(answer[1] < maxY) answer[1] += 1;
+                    break;
+                    
+                case "down":
+                    if(answer[1] > -maxY) answer[1] -= 1;
+                    break;
+                    
+                case "left":
+                    if(answer[0] > -maxX) answer[0] -= 1;
+                    break;
+                    
+                case "right":
+                    if(answer[0] < maxX) answer[0] += 1;
+                    break;
+            }
+        }
+        
+        return answer;
+    }
+}
