f9ca19343bc11f82cb464a09c1173c436b0f17fb
Feb <144503463+febCho@users.noreply.github.com>
2024-07-06 15:56:25 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.01 ms, Memory: 66 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int sum = 0;
+        
+        for(int n : numbers){
+            sum += n;
+        }
+        
+        return 45 - sum;
+    }
+}
